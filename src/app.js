abb6e3ef4a0b259785a648b82282a3b09cf3ce47
Ricardo Franco Pérez <rfrancop01@gmail.com>
2024-11-13 05:16:08 +0000

    [Sp-91] feat: Generador de excusas terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['Mi vecina', 'Mi perro', 'El perro de mi vecina', 'El ratón',];
+  let action = ['se comió', 'cortó', 'quemó', 'cocinó',];
+  let what = ['mi proyecto', 'la silla', 'el teclado', 'el html',];
+  let when = ['hoy', 'ayer', 'durante el paseo', 'la semana pasada',];
+
+  let excuse1 = Math.floor(Math.random() * who.length);
+  let excuse2 = Math.floor(Math.random() * action.length);
+  let excuse3 = Math.floor(Math.random() * what.length);
+  let excuse4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector(".excuse").innerHTML = 
+    who[excuse1] + ' ' + action[excuse2] + ' ' + what[excuse3] + ' ' + when[excuse4]
 };
+
